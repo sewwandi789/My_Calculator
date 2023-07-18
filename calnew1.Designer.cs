90058caa99c1c2d2f4a68c93ec6138b90a3f27f2
sewwandi789 <117725276+sewwandi789@users.noreply.github.com>
2023-07-18 12:25:34 +0530

    Add files via upload

---
+++ calnew1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Calculator
+{
+    partial class calnew1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdequal = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.cmdclear = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.cmddevided = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.cmdpresentage = new System.Windows.Forms.Button();
+            this.cmdmultiply = new System.Windows.Forms.Button();
+            this.cmdsubstract = new System.Windows.Forms.Button();
+            this.cmd = new System.Windows.Forms.Button();
+            this.txtvalue = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.cmdequal);
+            this.groupBox1.Controls.Add(this.btn0);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Controls.Add(this.cmdclear);
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.cmddevided);
+            this.groupBox1.Controls.Add(this.btn9);
+            this.groupBox1.Controls.Add(this.btn8);
+            this.groupBox1.Controls.Add(this.btn7);
+            this.groupBox1.Controls.Add(this.cmdpresentage);
+            this.groupBox1.Controls.Add(this.cmdmultiply);
+            this.groupBox1.Controls.Add(this.cmdsubstract);
+            this.groupBox1.Controls.Add(this.cmd);
+            this.groupBox1.Controls.Add(this.txtvalue);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 334);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // cmdequal
+            // 
+            this.cmdequal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdequal.Location = new System.Drawing.Point(157, 241);
+            this.cmdequal.Name = "cmdequal";
+            this.cmdequal.Size = new System.Drawing.Size(44, 68);
+            this.cmdequal.TabIndex = 34;
+            this.cmdequal.Text = "=";
+            this.cmdequal.UseVisualStyleBackColor = true;
+            this.cmdequal.Click += new System.EventHandler(this.cmdequal_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(28, 275);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(123, 34);
+            this.btn0.TabIndex = 33;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(114, 240);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(37, 29);
+            this.btn3.TabIndex = 32;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(71, 240);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(37, 29);
+            this.btn2.TabIndex = 31;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(28, 240);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(37, 29);
+            this.btn1.TabIndex = 30;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // cmdclear
+            // 
+            this.cmdclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdclear.Location = new System.Drawing.Point(157, 203);
+            this.cmdclear.Name = "cmdclear";
+            this.cmdclear.Size = new System.Drawing.Size(44, 32);
+            this.cmdclear.TabIndex = 29;
+            this.cmdclear.Text = "C";
+            this.cmdclear.UseVisualStyleBackColor = true;
+            this.cmdclear.Click += new System.EventHandler(this.cmdclear_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(114, 205);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(37, 29);
+            this.btn6.TabIndex = 28;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(71, 205);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(37, 29);
+            this.btn5.TabIndex = 27;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(28, 205);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(37, 29);
+            this.btn4.TabIndex = 26;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // cmddevided
+            // 
+            this.cmddevided.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmddevided.Location = new System.Drawing.Point(157, 170);
+            this.cmddevided.Name = "cmddevided";
+            this.cmddevided.Size = new System.Drawing.Size(44, 32);
+            this.cmddevided.TabIndex = 25;
+            this.cmddevided.Text = "/";
+            this.cmddevided.UseVisualStyleBackColor = true;
+            this.cmddevided.Click += new System.EventHandler(this.cmddevided_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(114, 170);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(37, 29);
+            this.btn9.TabIndex = 24;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(71, 170);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(37, 29);
+            this.btn8.TabIndex = 23;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(28, 170);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(37, 29);
+            this.btn7.TabIndex = 22;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // cmdpresentage
+            // 
+            this.cmdpresentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdpresentage.Location = new System.Drawing.Point(157, 132);
+            this.cmdpresentage.Name = "cmdpresentage";
+            this.cmdpresentage.Size = new System.Drawing.Size(44, 32);
+            this.cmdpresentage.TabIndex = 21;
+            this.cmdpresentage.Text = "%";
+            this.cmdpresentage.UseVisualStyleBackColor = true;
+            this.cmdpresentage.Click += new System.EventHandler(this.cmdpresentage_Click);
+            // 
+            // cmdmultiply
+            // 
+            this.cmdmultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdmultiply.Location = new System.Drawing.Point(114, 132);
+            this.cmdmultiply.Name = "cmdmultiply";
+            this.cmdmultiply.Size = new System.Drawing.Size(37, 32);
+            this.cmdmultiply.TabIndex = 20;
+            this.cmdmultiply.Text = "*";
+            this.cmdmultiply.UseVisualStyleBackColor = true;
+            this.cmdmultiply.Click += new System.EventHandler(this.cmdmultiply_Click);
+            // 
+            // cmdsubstract
+            // 
+            this.cmdsubstract.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdsubstract.Location = new System.Drawing.Point(71, 132);
+            this.cmdsubstract.Name = "cmdsubstract";
+            this.cmdsubstract.Size = new System.Drawing.Size(37, 32);
+            this.cmdsubstract.TabIndex = 19;
+            this.cmdsubstract.Text = "-";
+            this.cmdsubstract.UseVisualStyleBackColor = true;
+            this.cmdsubstract.Click += new System.EventHandler(this.cmdsubstract_Click);
+            // 
+            // cmd
+            // 
+            this.cmd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd.Location = new System.Drawing.Point(28, 132);
+            this.cmd.Name = "cmd";
+            this.cmd.Size = new System.Drawing.Size(37, 32);
+            this.cmd.TabIndex = 18;
+            this.cmd.Text = "+";
+            this.cmd.UseVisualStyleBackColor = true;
+            this.cmd.Click += new System.EventHandler(this.cmdadd_Click);
+            // 
+            // txtvalue
+            // 
+            this.txtvalue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalue.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtvalue.Location = new System.Drawing.Point(6, 19);
+            this.txtvalue.Multiline = true;
+            this.txtvalue.Name = "txtvalue";
+            this.txtvalue.Size = new System.Drawing.Size(210, 88);
+            this.txtvalue.TabIndex = 17;
+            // 
+            // calnew1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 357);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "calnew1";
+            this.Text = "calnew1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtvalue;
+        private System.Windows.Forms.Button cmd;
+        private System.Windows.Forms.Button cmdsubstract;
+        private System.Windows.Forms.Button cmdmultiply;
+        private System.Windows.Forms.Button cmdpresentage;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button cmddevided;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button cmdclear;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button cmdequal;
+    }
+}
---
+++ calnew1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Calculator
+{
+    public partial class calnew1 : Form
+    {
+        
+        double FirstNumber;
+        string Operation; 
+        public calnew1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtvalue.Text = txtvalue.Text + "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "1";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "1";
+            }  
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "2";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "2";
+            }  
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "3";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "3";
+            }  
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "4";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "4";
+            }  
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "5";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "5";
+            }  
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "6";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "6";
+            }  
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "7";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "7";
+            }  
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "8";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "8";
+            }  
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (txtvalue.Text == "0" && txtvalue.Text != null)
+            {
+                txtvalue.Text = "9";
+            }
+            else
+            {
+                txtvalue.Text = txtvalue.Text + "9";
+            }  
+        }
+
+        private void cmdclear_Click(object sender, EventArgs e)
+        {
+                txtvalue.Text = " ";
+        }
+
+        private void cmdadd_Click(object sender, EventArgs e)
+        {
+            double FirstNumber = Convert.ToDouble(txtvalue.Text);
+            txtvalue.Text = " ";
+            Operation = " + ";              
+        }
+
+        private void cmdequal_Click(object sender, EventArgs e)
+        {
+            double SecondNumber;
+            double Result;
+
+             SecondNumber = Convert.ToDouble(txtvalue.Text);
+
+            if (Operation == " + ")
+            {
+                Result = (FirstNumber + SecondNumber);
+                txtvalue.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+
+            }
+            if (Operation == " * ")
+            {
+                Result = (FirstNumber * SecondNumber);
+                txtvalue.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+            }
+            if (Operation == " % ")
+            {
+                if (FirstNumber != 0 && SecondNumber == null)
+                {
+                    Result = (FirstNumber / 100);
+                    txtvalue.Text = Convert.ToString(Result);
+                    FirstNumber = Result;
+                }
+            }
+            if (Operation == " / ")
+            {
+                if(SecondNumber==0)
+                {
+                    txtvalue.Text = "Invalid operation";   
+                }
+                else
+                {
+                Result = (FirstNumber / SecondNumber);
+                txtvalue.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+                }
+            }
+            if ( Operation == " - ")
+            {
+                Result = (FirstNumber - SecondNumber);
+                txtvalue.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+            }
+            txtvalue.Text = Convert.ToString(FirstNumber);
+        }
+
+        private void cmdmultiply_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(txtvalue.Text);
+            txtvalue.Text = " ";
+            Operation = " * ";  
+        }
+
+        private void cmdsubstract_Click(object sender, EventArgs e)
+        {      
+            FirstNumber = Convert.ToDouble(txtvalue.Text);
+            txtvalue.Text = " ";
+            Operation = " - ";  
+        }
+
+        private void cmdpresentage_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(txtvalue.Text);
+            txtvalue.Text = " ";
+            Operation = " % ";  
+        }
+
+        private void cmddevided_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(txtvalue.Text);
+            txtvalue.Text = " ";
+            Operation = " / ";  
+        }
+
+
+
+
+
+
+    }
+}
